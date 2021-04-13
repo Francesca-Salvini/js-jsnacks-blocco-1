0180b3656ec93e912067605c7c2ea1cb42ef99bb
Francesca Salvini <franci98sp@gmail.com>
2021-04-13 15:22:21 +0200

    impostato secondo esercizio

--- script.js
+++ script.js
@@ -5,74 +5,74 @@
 // indicandone per ognuno varietà, peso e lunghezza. 
 // Calcola la somma del peso di tutte le zucchine.
 
-var arrayZucchine = [
-    {
-        "varieta" : "Zucchino nero di Milano" ,
-        "peso" : 200,
-        "lunghezza" : 20 
-    },
-    {
-        "varieta" : "Zucchino romanesco" ,
-        "peso" : 150,
-        "lunghezza" : 12 
-    },
-    {
-        "varieta" : "Zucchino ortolano di Faenza" ,
-        "peso" : 120,
-        "lunghezza" : 10 
-    },
-    {
-        "varieta" : "Zucchina lunga fiorentina" ,
-        "peso" : 70,
-        "lunghezza" : 30 
-    },
-    {
-        "varieta" : "Zucchino siciliano" ,
-        "peso" : 320,
-        "lunghezza" : 60 
-    },
-    {
-        "varieta" : "Zucchina striata di Napoli" ,
-        "peso" : 100,
-        "lunghezza" : 14 
-    },
-    {
-        "varieta" : "Zucchina bianca triestina" ,
-        "peso" : 80,
-        "lunghezza" : 20 
-    },
-    {
-        "varieta" : "Zucchina rigata pugliese" ,
-        "peso" : 115,
-        "lunghezza" : 16 
-    },
-    {
-        "varieta" : "Zucchino tondo di Piacenza" ,
-        "peso" : 90,
-        "lunghezza" : 12
-    },
-    {
-        "varieta" : "Zucchino tondo di Nizza" ,
-        "peso" : 50,
-        "lunghezza" : 6 
-    }
-];
+// var arrayZucchine = [
+//     {
+//         "varieta" : "Zucchino nero di Milano" ,
+//         "peso" : 200,
+//         "lunghezza" : 20 
+//     },
+//     {
+//         "varieta" : "Zucchino romanesco" ,
+//         "peso" : 150,
+//         "lunghezza" : 12 
+//     },
+//     {
+//         "varieta" : "Zucchino ortolano di Faenza" ,
+//         "peso" : 120,
+//         "lunghezza" : 10 
+//     },
+//     {
+//         "varieta" : "Zucchina lunga fiorentina" ,
+//         "peso" : 70,
+//         "lunghezza" : 30 
+//     },
+//     {
+//         "varieta" : "Zucchino siciliano" ,
+//         "peso" : 320,
+//         "lunghezza" : 60 
+//     },
+//     {
+//         "varieta" : "Zucchina striata di Napoli" ,
+//         "peso" : 100,
+//         "lunghezza" : 14 
+//     },
+//     {
+//         "varieta" : "Zucchina bianca triestina" ,
+//         "peso" : 80,
+//         "lunghezza" : 20 
+//     },
+//     {
+//         "varieta" : "Zucchina rigata pugliese" ,
+//         "peso" : 115,
+//         "lunghezza" : 16 
+//     },
+//     {
+//         "varieta" : "Zucchino tondo di Piacenza" ,
+//         "peso" : 90,
+//         "lunghezza" : 12
+//     },
+//     {
+//         "varieta" : "Zucchino tondo di Nizza" ,
+//         "peso" : 50,
+//         "lunghezza" : 6 
+//     }
+// ];
 
-var somma = 0;
-// console.log(arrayZucchine);
-for(var i = 0; i < arrayZucchine.length; i++){
-    var thisZucchina = arrayZucchine[i];
-    // console.log(thisZucchina);
+// var somma = 0;
+// // console.log(arrayZucchine);
+// for(var i = 0; i < arrayZucchine.length; i++){
+//     var thisZucchina = arrayZucchine[i];
+//     // console.log(thisZucchina);
 
-    var pesoZucchine = thisZucchina.peso;
-    console.log(pesoZucchine);
+//     var pesoZucchine = thisZucchina.peso;
+//     console.log(pesoZucchine);
 
-    somma += pesoZucchine;
+//     somma += pesoZucchine;
     
-}
+// }
 
     
-console.log("Il peso complessivo delle zucchine è: " + somma);
+// console.log("Il peso complessivo delle zucchine è: " + somma);
  
 // FINE PRIMO ESERCIZIO
 
@@ -81,6 +81,78 @@ console.log("Il peso complessivo delle zucchine è: " + somma);
 // Dividi in due array separati le zucchine che misurano meno o più di 15cm.
 // Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 
+var arrayZucchine = [
+        {
+            "varieta" : "Zucchino nero di Milano" ,
+            "peso" : 200,
+            "lunghezza" : 20 
+        },
+        {
+            "varieta" : "Zucchino romanesco" ,
+            "peso" : 150,
+            "lunghezza" : 12 
+        },
+        {
+            "varieta" : "Zucchino ortolano di Faenza" ,
+            "peso" : 120,
+            "lunghezza" : 10 
+        },
+        {
+            "varieta" : "Zucchina lunga fiorentina" ,
+            "peso" : 70,
+            "lunghezza" : 30 
+        },
+        {
+            "varieta" : "Zucchino siciliano" ,
+            "peso" : 320,
+            "lunghezza" : 60 
+        },
+        {
+            "varieta" : "Zucchina striata di Napoli" ,
+            "peso" : 100,
+            "lunghezza" : 14 
+        },
+        {
+            "varieta" : "Zucchina bianca triestina" ,
+            "peso" : 80,
+            "lunghezza" : 20 
+        },
+        {
+            "varieta" : "Zucchina rigata pugliese" ,
+            "peso" : 115,
+            "lunghezza" : 16 
+        },
+        {
+            "varieta" : "Zucchino tondo di Piacenza" ,
+            "peso" : 90,
+            "lunghezza" : 12
+        },
+        {
+            "varieta" : "Zucchino tondo di Nizza" ,
+            "peso" : 50,
+            "lunghezza" : 6 
+        }
+    ];
+
+    var menodiQuindici = [];
+    var piuDiQuindici = [];
+    console.log(arrayZucchine);
+
+    for(var i = 0; i < arrayZucchine.length; i++ ){
+        var thisZucchina = arrayZucchine[i];
+
+        console.log(thisZucchina);
+        if (thisZucchina.lunghezza < 15) {
+            menodiQuindici.push(thisZucchina);
+        } else {
+            piuDiQuindici.push(thisZucchina);
+        }
+
+        
+    }
+
+    console.log("meno di quindici " + menodiQuindici);
+    console.log("piu di quindici " + piuDiQuindici);
 
 
 // FINE SECONDO ESERCIZIO
