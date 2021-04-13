a039267d2c7701f673e8d4f3bef98c14b34bf9fb
Francesca Salvini <franci98sp@gmail.com>
2021-04-13 14:38:17 +0200

    creata infrastruttura

---
+++ script.js
@@ -0,0 +1,11 @@
+// CONSEGNA
+
+// PRIMO ESERCIZIO 
+// Crea 10 oggetti che rappresentano una zucchina, 
+// indicandone per ognuno varietà, peso e lunghezza. 
+// Calcola la somma del peso di tutte le zucchine.
+
+// SECONDO ESERCIZIO
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
